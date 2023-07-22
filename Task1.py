ca85958b55f0d11b052c8eca338b731c87304df1
Liubov <lyubasik85@rambler.ru>
2023-07-22 15:45:54 +0300

    Треугольник

---
+++ Task1.py
@@ -0,0 +1,16 @@
+def triagle (a, b, c):
+    if (a + b < c or a + c < b or b + c < a):
+       return 'Треугольника c такими сторонами не существует!'   
+
+    if (a == b and b == c and c == a):
+        return 'Треугольник равносторонний.'
+    elif (a != b and a != c and b != c):
+        return 'Треугольник разносторонний.'  
+    else:
+        return 'Треугольник равнобедренный.'  
+
+
+print(triagle(4, 4, 4))
+print(triagle(4, 5, 6))
+print(triagle(4, 4, 8))
+print(triagle(4, 4, 12))
